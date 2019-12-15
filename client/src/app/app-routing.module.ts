a4190b138dfe03ec910972c8956b45989af37868
roma kapa <roma.kpv@gmail.com>
2019-12-15 19:19:10 +0200

    add client auth jwt

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './service/auth.guard';
 
-const routes: Routes = [];
+import { ProductComponent } from './product/product-grid.component';
+import { HomeComponent } from './home/home/home.component';
+import { LoginComponent } from './service/login/login.component';
+
+
+const routes: Routes = [ 
+  { path: 'product', component: ProductComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: '',component: HomeComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,54 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
-import { JwtModule } from "@auth0/angular-jwt";
 import { FormsModule } from '@angular/forms';
+
+import { JwtModule } from "@auth0/angular-jwt";
+import { AuthService } from './service/auth.service';
+import { AuthGuard } from './service/auth.guard';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './shared/main-menu.component';
 import { ProductComponent } from './product/product-grid.component';
-import { LoginComponent } from './account/login/login.component';
 import { HomeComponent } from './home/home/home.component';
-import { AuthComponent } from './auth/auth/auth.component';
-
-import { AuthGuard } from './auth/quard/auth-guard.service';
+import { LoginComponent } from './service/login/login.component';
 
 export function tokenGetter() {
   return localStorage.getItem("jwt");
 }
-// определение дочерних маршрутов
-const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'product', component: ProductComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'auth', component: AuthComponent, canActivate: [AuthGuard]}
-];
 
 @NgModule({
   declarations: [
     AppComponent,
     MainMenuComponent,
     ProductComponent,
-    LoginComponent,
     HomeComponent,
-    AuthComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes),
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
-        whitelistedDomains: ["localhost:44330"],
-        blacklistedRoutes: []
+        whitelistedDomains: ['localhost:44330'],
+        blacklistedRoutes: ['localhost:44330/api/login']
       }
     })
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
