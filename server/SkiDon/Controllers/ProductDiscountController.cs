0765a1ae73962b5b16b7cb0f93943c3035dea3da
Andrii Liashenko <andrey.lyashenko@lizard-soft.com>
2020-01-12 19:13:14 +0200

    add api for chek discounts

---
+++ server/SkiDon/Controllers/ProductDiscountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SkiDon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductDiscountController : ControllerBase
+    {
+        private readonly SkidonContext _context;
+
+        public ProductDiscountController(SkidonContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IQueryable<ProductsDiscount>>> Get([FromQuery]long productId)
+        {
+            return Ok(_context.ProductsDiscounts.Where(x => x.ProductId == productId));
+        }
+    }
+}
